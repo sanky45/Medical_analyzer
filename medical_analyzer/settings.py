56a1d734b38fd886c433449cf46978c6542d9d4f
sanky45 <yadavsb26@gmail.com>
2025-07-13 15:06:39 +0530

    Render deployment: static files, WhiteNoise, dependency cleanup, render.yaml

--- medical_analyzer/settings.py
+++ medical_analyzer/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise after SecurityMiddleware
 ]
 
 ROOT_URLCONF = 'medical_analyzer.urls'
@@ -128,6 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
